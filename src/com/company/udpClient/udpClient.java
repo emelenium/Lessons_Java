788e9eda319451ace132b48b2fb4c2053931c7cf
master <emelenium@gmail.com>
2019-11-12 21:07:45 +0500

    Исправление ошибок

--- src/com/company/udpClient/udpClient.java
+++ src/com/company/udpClient/udpClient.java
@@ -6,19 +6,21 @@ import java.net.InetAddress;
 
 public class udpClient {
     public static void main(String[] args) throws Exception {
+
+        DatagramSocket ds = new DatagramSocket();
+
         int i = 8;
-        byte[] bytes = (i + "").getBytes();
+        byte[] b = String.valueOf(i).getBytes();
 
-        DatagramSocket datagramSocket = new DatagramSocket();
-        InetAddress inetAddress = InetAddress.getLocalHost();
-        DatagramPacket datagramPacket = new DatagramPacket(bytes, bytes.length, inetAddress, 9999);
-        datagramSocket.send(datagramPacket);
+        InetAddress ia = InetAddress.getLocalHost();
+        DatagramPacket dp = new DatagramPacket(b, b.length, ia, 9999);
+        ds.send(dp);
 
-        byte[] bytes1 = new byte[1024];
-        DatagramPacket datagramPacket1 = new DatagramPacket(bytes1,bytes1.length);
-        datagramSocket.receive(datagramPacket1);
+        byte[] b1 = new byte[1024];
+        DatagramPacket dp1 = new DatagramPacket(b1,b1.length);
+        ds.receive(dp1);
 
-        String str = new String((datagramPacket1.getData()));
+        String str = new String((dp1.getData()));
         System.out.println("result is " + str);
 
     }
