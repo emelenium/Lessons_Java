47eb8669a0966ae50fdff6bfe2298cb9cfeb9ec8
anton <emelenium@gmail.com>
2019-11-14 21:00:16 +0500

    редактирование gitignore

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Socket socket = new Socket();
+        SocketAddress socketAddress = new InetSocketAddress("india.colorado.edu",13);
+
+        //        socket.connect(new InetSocketAddress("india.colorado.edu",13));
+
+
     }
 }
