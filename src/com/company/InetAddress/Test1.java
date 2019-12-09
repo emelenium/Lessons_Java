3e6d7a1c398113749c0a40b5b756752a4e61d14a
master <emelenium@gmail.com>
2019-12-09 21:33:34 +0500

    Добавлены функция InetAddress.getByName по имени и по адресу

--- src/com/company/InetAddress/Test1.java
+++ src/com/company/InetAddress/Test1.java
@@ -1,4 +1,23 @@
 package com.company.InetAddress;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Test1 {
+    public static void main(String[] args) {
+
+
+        try {
+            //Displaying host ip address (показывает ip адрес хоста)
+            InetAddress host = InetAddress.getByName("wwww.yahoo.com");
+            System.out.println("Host ip Address is :" + host.getHostAddress());
+
+            //Displaying host Name from ip address (показывает имя хоста по его ip адресу)
+            InetAddress host1 = InetAddress.getByName("212.82.100.151");
+        System.out.println("Host name for given ip is : "+host1.getHostName());
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
 }
