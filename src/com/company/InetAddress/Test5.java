400f8c70162f61575b37aca9fb6006c1ce91262c
emelenium <emelenium@gmail.com>
2020-03-24 21:14:08 +0500

    Test5.java Сетевые интерфейсы вывод в файд OutPut.txt

--- src/com/company/InetAddress/Test5.java
+++ src/com/company/InetAddress/Test5.java
@@ -1,5 +1,7 @@
 package com.company.InetAddress;
 
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
@@ -7,11 +9,15 @@ import java.util.Enumeration;
 public class Test5 {
     public static void main(String[] args) throws SocketException {
         //получает и выводит все сетевые интерфейсы
-        Enumeration interfaces = NetworkInterface.getNetworkInterfaces();
-        while(interfaces.hasMoreElements())
-        {
-            NetworkInterface in = (NetworkInterface)interfaces.nextElement();
-            System.out.println(in);
-        }
+        try {
+            Enumeration interfaces = NetworkInterface.getNetworkInterfaces();
+            System.setOut(new PrintStream(new FileOutputStream("OutPut.txt")));
+            while (interfaces.hasMoreElements())
+                {
+                NetworkInterface in = (NetworkInterface) interfaces.nextElement();
+                System.out.println(in);
+                }
+
+        }catch (Exception e){e.printStackTrace();}
     }
 }
