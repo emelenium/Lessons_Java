170043cb172c4af1bf4aa8a5e70aed7ead0983cd
emelenium <emelenium@gmail.com>
2020-03-24 20:30:10 +0500

    Test2.java

--- src/com/company/InetAddress/Test2.java
+++ src/com/company/InetAddress/Test2.java
@@ -8,8 +8,8 @@ public class Test2 {
         try {
             InetAddress address = InetAddress.getByName("127.0.0.1");
             if (address.isAnyLocalAddress()) System.out.println(address+" is wildcard address");
-            if (address.isLoopbackAddress()) System.out.println(address+" is a loopback address");
-            if (address.isLinkLocalAddress()) System.out.println(address+" is a link-local address");
+            else if (address.isLoopbackAddress()) System.out.println(address+" is a loopback address");
+            else if (address.isLinkLocalAddress()) System.out.println(address+" is a link-local address");
             else if (address.isSiteLocalAddress()) System.out.println(address+" is site local address");
             else System.out.println(address+" is global address");
         }catch (UnknownHostException e){e.printStackTrace();}
