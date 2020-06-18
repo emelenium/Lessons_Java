da41ddf10b1300d726f653ca2703127f654899f2
emelenium <emelenium@gmail.com>
2020-06-18 22:08:16 +0500

    коментарий

--- src/com/company/mysql/mysqlMain.java
+++ src/com/company/mysql/mysqlMain.java
@@ -4,14 +4,19 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class mysqlMain {
 public static void main(String[] args) throws ParserConfigurationException, ClassNotFoundException, SQLException {
     String userName = "rc";
     String password = "tu@zvc2A11i098";
     String connectUrl = "jdbc:mysql://192.168.115.128:3306/test";
-    Class.forName("com.mysql.cj.jdbc.Driver");
-    try(Connection connection = DriverManager.getConnection(connectUrl,userName,password)) {
+   // Class.forName("com.mysql.cj.jdbc.Driver");
+    //Когда метод getConnection вызывается, DriverManager попытается найти подходящий драйвер из числа тех,
+    // которые загружены при инициализации, и тех, которые загружены явно, используя тот же загрузчик классов,
+    // что и текущий апплет или приложение.
+        try(Connection connection = DriverManager.getConnection(connectUrl,userName,password);
+        Statement statement = connection.createStatement()) {
         System.out.println("We're connected");
     }
 }
